6af71b5b6b544988fc4647bfd8279925954e6a25
Woong Ze Yi <woongzeyi@gmail.com>
2021-10-09 08:59:57 +0800

    Implement generated code saving

--- image_to_skill/__main__.py
+++ image_to_skill/__main__.py
@@ -1,19 +1,18 @@
-import os
-from os.path import isfile, join
+from os import makedirs, listdir, getcwd
+from os.path import isfile, join, splitext
 from typing import Dict, List
 from filetype import is_image
-from filetype.types import image
 from .image_processor import ImageDetails, get_color_details_from_image
 from .code_generation import generate_code
 
 def main():
     # Images need to be put into the `./images` directory
-    images_directory: str = os.getcwd() + "\\images"
-    os.makedirs(images_directory, exist_ok = True)
+    images_directory: str = getcwd() + "\\images"
+    makedirs(images_directory, exist_ok = True)
 
     # Opening a non-image file with PIL will throw an exception
     images: List[str] = [f 
-        for f in os.listdir(images_directory) 
+        for f in listdir(images_directory) 
         if isfile(join(images_directory, f)) 
         if is_image(join(images_directory, f))
     ]
@@ -25,8 +24,11 @@ def main():
     image_to_code: Dict[str, str] = {}
     for key, value in image_to_details.items():
         image_to_code[key] = generate_code(value)
+
     for key, value in image_to_code.items():
-        print("{}: \n{}".format(key, value))
+        with open(join(images_directory, splitext(key)[0] + ".yaml"), 'w') as yaml_file:
+            yaml_file.write(value)
+
 
 if __name__ == "__main__":
     main()
